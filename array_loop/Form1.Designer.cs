74997c58df8189282c694f068614e8a393abf766
renjith-1989 <renjithkuniyillath@yahoo.in>
2015-03-26 07:02:14 +0530

    first commit
    first commit

---
+++ array_loop/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace array_loop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.ListBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.FormattingEnabled = true;
+            this.lb1.Location = new System.Drawing.Point(38, 81);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(172, 134);
+            this.lb1.TabIndex = 0;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(71, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(139, 36);
+            this.b1.TabIndex = 1;
+            this.b1.Text = "button1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
